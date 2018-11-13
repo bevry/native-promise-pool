e9525c9b8e56724651dee63602ae4c3ba3cd4cae
Benjamin Lupton <b@lupton.cc>
2018-11-14 01:31:06 +1100

    added error test

--- source/index.js
+++ source/index.js
@@ -1,3 +1,42 @@
 'use strict'
 
-module.exports = require('./all.js')
+module.exports = class ArrayPromisePool {
+	static create (...args) {
+		return new this(...args)
+	}
+
+	constructor (concurrency) {
+		this.concurrency = concurrency
+		this.running = 0
+		this.started = 0
+		this.queue = []
+	}
+
+	/**
+	 * Add a task to the pool
+	 * @param {Function<Promise>} task
+	 * @return {Promise} promise
+	 */
+	open (task) {
+		const p = new Promise((resolve) => this.queue.push(resolve))
+			.finally(() => {
+				this.started--
+				this.running++
+			})
+			.then(task)
+			.finally(() => {
+				this.running--
+				if (this.queue.length) {
+					this.started++
+					this.queue.shift()()
+				}
+			})
+
+		if ((this.running + this.started) < this.concurrency && this.queue.length) {
+			this.started++
+			this.queue.shift()()
+		}
+
+		return p
+	}
+}
--- source/test.js
+++ source/test.js
@@ -3,36 +3,48 @@
 const { equal, deepEqual } = require('assert-helpers')
 const joe = require('joe')
 
-const PromisePool = require('./all.js')
+const PromisePool = require('./index.js')
+
+const pools = 2
+const concurrency = 2
 
 const tasks = [
 	() => new Promise((resolve) => setTimeout(() => resolve('task 1'), 1000)),
 	() => 'task 2',
+	() => new Promise((resolve, reject) => reject('intended rejection')),
 	() => new Promise((resolve) => setTimeout(() => resolve('task 3'), 1000)),
 	() => new Promise((resolve) => setTimeout(() => resolve('task 4'), 1000)),
 	() => 'task 5',
 	() => 'task 6'
 ]
-const expectedOL = ['task 1', 'task 2', 'task 3', 'task 4', 'task 5', 'task 6']
-const expectedUL = ['task 2', 'task 1', 'task 3', 'task 5', 'task 6', 'task 4']
+const expectedOL = ['task 1', 'task 2', 'intended rejection', 'task 3', 'task 4', 'task 5', 'task 6']
+const expectedUL = ['task 2', 'intended rejection', 'task 1', 'task 3', 'task 5', 'task 6', 'task 4']
 
 joe.suite('native-promise-pool', function (suite, test) {
-	test('array works', function (done) {
-		const pool = PromisePool.create(2)
-		const ul = []
-		Promise.all(
-			tasks.map((task) => pool.open(task).then(function (result) {
-				ul.push(result)
-				console.log(result)
-				return result
-			}))
-		).then(function (ol) {
-			deepEqual(ol, expectedOL, 'ordered results')
-			deepEqual(ul, expectedUL, 'unordered results')
-			equal(pool.running, 0, 'running is empty')
-			equal(pool.started, 0, 'started is empty')
-			deepEqual(pool.queue, [], 'queue is empty')
-			done()
-		})
+	const pool = PromisePool.create(2)
+	suite('works', function (suite, test) {
+		for (let i = 0; i < pools; ++i) {
+			test(`pool ${i}`, function (done) {
+				const ul = []
+				Promise.all(
+					tasks.map((task) => pool.open(task).then(function (result) {
+						ul.push(result)
+						console.log(result)
+						return result
+					}).catch(function (err) {
+						ul.push(err.message || err)
+						console.error(err)
+						return err
+					}))
+				).then(function (ol) {
+					deepEqual(ol, expectedOL, 'ordered results')
+					deepEqual(ul, expectedUL, 'unordered results')
+					equal(pool.running, 0, 'running is empty')
+					equal(pool.started, 0, 'started is empty')
+					deepEqual(pool.queue, [], 'queue is empty')
+					done()
+				})
+			})
+		}
 	})
 })
