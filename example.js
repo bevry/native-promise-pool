a247e438fb4a9094cba66e5c4d0ff7ff302b0976
Benjamin Lupton <b@lupton.cc>
2018-11-14 01:19:03 +1100

    updated example with live updating table

--- example.js
+++ example.js
@@ -1,25 +1,56 @@
 'use strict'
 
 const chalk = require('chalk')
-const PromisePool = require('./')
+const logger = require('logger-clearable').create()
 
+const logs = []
+const statuses = {}
 function time () {
 	const t = new Date()
 	return t.getMinutes() + ':' + t.getSeconds() + ':' + t.getMilliseconds()
 }
+function status (i, status) {
+	logs.push(status)
+	statuses[i] = status
+	logger.log(
+		// '== LOGS ==\n' +
+		// logs.join('\n') +
+		// '\n\n' +
+		'== STATUS ===\n' +
+		Object.values(statuses).join('\n')
+	)
+}
+function message (heading, message) {
+	logger.log(
+		// '== LOGS ==\n' +
+		// logs.join('\n') +
+		// '\n\n' +
+		'== STATUS ===\n' +
+		Object.values(statuses).join('\n') +
+		'\n\n' +
+		`== ${heading} ===\n` +
+		message
+	)
+}
 
-const pool = new PromisePool(2)
+const concurrency = 3
+const tasks = 10
+const delay = 1000
 
-for (let i = 0; i < 10; i++) {
-	pool.open(() => new Promise(function (resolve) {
-		const delay = 10000
+const PromisePool = require('./')
+const pool = new PromisePool(concurrency)
+
+Promise.all(
+	Array(10).fill(null).map((value, i) => pool.open(() => new Promise(function (resolve) {
 		const name = `Task ${i}\t\tdelay ${delay}ms`
-		console.log(time(), '\t' + chalk.green('started') + '\t\t' + name)
+		status(i, time() + '\t' + chalk.green('started') + '\t\t' + name)
 		setTimeout(function () {
-			console.log(time(), '\t' + chalk.red('finished') + '\t' + name)
+			status(i, time() + '\t' + chalk.red('finished') + '\t' + name)
 			resolve(name)
 		}, delay)
 	})).then(function (result) {
-		console.log(time(), '\t' + chalk.blue('result') + '\t\t' + result)
+		status(i, time() + '\t' + chalk.blue('result') + '\t\t' + result)
+		return result
 	})
-}
+	)
+).then((results) => message('DONE', results.join('\n')))
